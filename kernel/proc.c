abf09309158c360b04fb70da84da86e688449f9d
Ori <suchy@post.bgu.ac.il>
2021-05-09 21:05:00 +0300

    free trapframe backup from thread

--- kernel/proc.c
+++ kernel/proc.c
@@ -299,6 +299,8 @@ freeThread(struct thread *t)
 {
   if (t->trapframe)
     kfree((void *)t->trapframe);
+  if (t->userTrapBackup)
+    kfree((void *)t->userTrapBackup);
   //Is it the right way to free kstack?
   if (t->myNum != 0)
     kfree((void *)t->kstack);
